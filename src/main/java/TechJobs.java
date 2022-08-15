6394648a11eb974e45fe4f4c16289c35269dfdcc
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-08-14 20:17:44 -0500

    print jobs done

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,14 +120,12 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 //iterate over ArrayList someJobs of individual HashMap jobs with each object of individual key value pairs called jobEntry
-
         if (someJobs.size() > 0) {
             for (HashMap job : someJobs) {
-//            System.out.println("job" + job);
                 System.out.println("*****");
-                for (Object jobEntry : job.entrySet()) {
-                    System.out.println(jobEntry);
-                }
+                job.forEach((key, value) -> {
+                    System.out.println(key + ": " + value);
+                });
                 System.out.println("*****" + "\n");
             }
         } else {
@@ -136,3 +134,21 @@ public class TechJobs {
     }
 }
 
+//WORKS BUT GIVES OBJECT with = in print out:
+//    private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+////iterate over ArrayList someJobs of individual HashMap jobs with each object of individual key value pairs called jobEntry
+//
+//        if (someJobs.size() > 0) {
+//            for (HashMap job : someJobs) {
+////            System.out.println("job" + job);
+//                System.out.println("*****");
+//                for (Object jobEntry : job.entrySet()) {
+//                    System.out.println(jobEntry);
+//                }
+//                System.out.println("*****" + "\n");
+//            }
+//        } else {
+//            System.out.println("No Results");
+//        }
+//    }
+//}
