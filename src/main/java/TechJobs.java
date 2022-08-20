8faf8e9307c0e7715ee939663b011bac1830366b
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-08-20 16:20:29 -0500

    Final

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,7 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-//iterate over ArrayList someJobs of individual HashMap jobs with each job entry listing individual key value
+
         if (someJobs.size() > 0) {
             for (HashMap job : someJobs) {
                 System.out.println("\n" + "*****");
